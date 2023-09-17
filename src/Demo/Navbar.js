69db04b8b0c8887f139eb49b94e7298b76b81a23
Shivaji Jadhav <sj461898@gmail.com>
2023-09-18 00:44:25 +0530

    first

---
+++ src/Demo/Navbar.js
@@ -0,0 +1,99 @@
+import React from "react";
+import Options from "./Options";
+import myImg from "../Demo/plaksha.png";
+
+function Navbar() {
+  return (
+    <div>
+      <div
+        className="main"
+        style={{ backgroundColor: "rgb(0,85,90)", padding: 15 }}
+      >
+        <p style={{ color: "white", textAlign: "center" }}>
+          Sign up for Virtual Chats here. | View Academic Calendar 2023-24 here.
+          | Admissions for TLP Class of 2024 are closed. | Admissions to BTech
+          program are closed.{" "}
+        </p>
+      </div>
+      <nav className="navbar navbar-expand-lg bg-white">
+        <div className="container">
+          <a className="navbar-brand" href="##">
+            <img src={myImg} height={80} width={200} alt="logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myNavbar"
+          ></button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="myNavbar"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  href="##"
+                  style={{ color: "black", marginRight: "20px" }}
+                >
+                  Virtual Campus Tour
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="##"
+                  style={{ color: "black", marginRight: "20px" }}
+                >
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="##"
+                  style={{ color: "black", marginRight: "20px" }}
+                >
+                  my-Plaksha
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="##"
+                  style={{ color: "black", marginRight: "20px" }}
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="d-flex justify-content-end">
+            <div class="btn-group">
+              <button
+                class="btn btn-secondary btn-sm dropdown-toggle"
+                type="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                style={{
+                  backgroundColor: "rgb(0,85,90)",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                }}
+              >
+                APPLY NOW
+              </button>
+             
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Options />
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/Demo/Options.js
@@ -0,0 +1,293 @@
+import React from "react";
+import "./Options.css"
+
+function Options() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"rgb(0,85,90)",padding:"0%"}} >
+      <div className="container" >
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse  justify-content-center"
+          id="navbarNav"  
+        >
+          <ul className="navbar-nav"style={{gap:30}} >
+          <li className="nav-item dropdown" >
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                
+                <strong >ABOUT US</strong>
+              </a>
+              
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Vission mission & values
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Founders & Donors
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Academic Board
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                
+                <strong>PROGRAMS</strong>
+              </a>
+              
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Vission mission & values
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Founders & Donors
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Academic Board
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                
+                <strong>ADMISSION</strong>
+              </a>
+              
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Vission mission & values
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Founders & Donors
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Academic Board
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                
+                <strong>FACULTY</strong>
+              </a>
+              
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Vission mission & values
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Founders & Donors
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Academic Board
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="aboutDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                
+                <strong>RESEARCH</strong>
+              </a>
+              
+              <ul className="dropdown-menu" aria-labelledby="aboutDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Vission mission & values
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Founders & Donors
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Academic Board
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link "
+                href="##"
+              >
+                
+                <strong>STUDENT LIFE</strong>
+              </a>
+              
+           
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="##"
+                id="actionDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <strong>CAREER DEVELOPMENT</strong>
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="actionDropdown">
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="##">
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+         
+        </div>
+       
+      </div>
+    </nav>
+  );
+}
+
+export default Options;
